d9324faf9c6e6c25004111953c9ed8c2e49d4510
Magnetic_dud <mail@dandandin.it>
2014-12-05 23:35:53 +0100

    RTC start

---
+++ RTC_DS1307/RTC_DS1307.ino
@@ -0,0 +1,11 @@
+//using Keyes xd-207 - looks equivalent to this https://learn.adafruit.com/adafruit-data-logger-shield/overview
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
