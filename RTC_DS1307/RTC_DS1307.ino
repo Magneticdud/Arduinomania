32b73843ed08d373b24b9d3f40f45eea82a0f9c6
Magnetic_dud <mail@dandandin.it>
2014-12-08 01:09:54 +0100

    Indentation and slower serial

--- RTC_DS1307/RTC_DS1307.ino
+++ RTC_DS1307/RTC_DS1307.ino
@@ -7,10 +7,10 @@
 RTC_DS1307 RTC;
 
 void setup () {
-    Serial.begin(57600);
-    Wire.begin();
-    RTC.begin();
-
+  Serial.begin(9600);
+  
+  Wire.begin();
+  RTC.begin();
   if (! RTC.isrunning()) {
     Serial.println("RTC is NOT running!");
     // following line sets the RTC to the date & time this sketch was compiled
