f41fb4ceba9461d38c0474ed346cd0b30432c860
Magneticdud <Magneticdud@users.noreply.github.com>
2014-12-13 00:45:10 +0100

    Much smoother animation

--- NokiaLCD/NokiaLCD.ino
+++ NokiaLCD/NokiaLCD.ino
@@ -163,7 +163,7 @@ void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
       display.drawBitmap(icons[f][XPOS], icons[f][YPOS], logo16_glcd_bmp, w, h, BLACK);
     }
     display.display();
-    delay(200);
+    //delay(200);
     
     // then erase it + move it
     for (uint8_t f=0; f< NUMFLAKES; f++) {
