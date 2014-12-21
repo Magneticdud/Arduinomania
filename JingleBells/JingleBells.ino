6a077dc6a9e70b1db0bf9ffd5a48d15af46839ca
Magneticdud <Magneticdud@users.noreply.github.com>
2014-12-21 11:52:35 +0100

    I prefer starting from scratch

--- JingleBells/JingleBells.ino
+++ JingleBells/JingleBells.ino
@@ -1,49 +1,11 @@
-//from https://gist.github.com/elubow/7844436
-
+const int ledPin =  13;
 int speakerPin = 5;
-int length = 29; // the number of notes
-char notes[] = "eeeeeeegcdefffffeeeeddedg"; // a space represents a rest
-int beats[] = { 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1 };
-int tempo = 250;
 
 void setup() {
   pinMode(speakerPin, OUTPUT);
-  playmelody();
+  pinMode(ledPin, OUTPUT);
 }
+
 void loop() {
 
-}
-
-void playmelody() {
-  for (int i = 0; i < length; i++) {
-    if (notes[i] == ' ') {
-      delay(beats[i] * tempo); // rest
-    } else {
-      playNote(notes[i], beats[i] * tempo);
-    }
-    
-    // pause between notes
-    delay(tempo / 2); 
-  }
-}
-
-void playTone(int tone, int duration) {
-  for (long i = 0; i < duration * 1000L; i += tone * 2) {
-    digitalWrite(speakerPin, HIGH);
-    delayMicroseconds(tone);
-    digitalWrite(speakerPin, LOW);
-    delayMicroseconds(tone);
-  }
-}
-
-void playNote(char note, int duration) {
-  char names[] = { 'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C' };
-  int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
-  
-  // play the tone corresponding to the note name
-  for (int i = 0; i < 8; i++) {
-    if (names[i] == note) {
-      playTone(tones[i], duration);
-    }
-  }
 }
