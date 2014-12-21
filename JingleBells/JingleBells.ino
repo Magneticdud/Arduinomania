b92231b6e3ab52492d46ed32a68b74b0bb4776c5
Magneticdud <Magneticdud@users.noreply.github.com>
2014-12-21 12:06:19 +0100

    Nice!

--- JingleBells/JingleBells.ino
+++ JingleBells/JingleBells.ino
@@ -3,6 +3,8 @@
 const int ledPin =  13;
 int speakerPin = 5;
 
+#include "tone.h"
+
 //tempo
 int eighth = 0;
 int quarter = 2;
@@ -13,7 +15,7 @@ int whole = 8;
 void setup() {
   pinMode(speakerPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
-  setuptempo(250);
+  setuptempo(100);
   playmelody();
 }
 
@@ -22,7 +24,73 @@ void loop() {
 }
 
 void playmelody(){
+  /* jingle bells */
+  
   playnote(NOTE_E4,quarter);
+  playnote(NOTE_E4,quarter);
+  playnote(NOTE_E4,half);
+  
+  playnote(NOTE_E4,quarter);
+  playnote(NOTE_E4,quarter);
+  playnote(NOTE_E4,half);
+  
+  playnote(NOTE_E4,quarter);
+  playnote(NOTE_G4,quarter);
+  playnote(NOTE_C4,dottedquarter);
+  playnote(NOTE_D4,eighth);
+  
+  playnote(NOTE_E4,whole);
+  
+  playnote(NOTE_F4,quarter);
+  playnote(NOTE_F4,quarter);
+  playnote(NOTE_F4,quarter);
+  playnote(NOTE_F4,quarter);
+  
+  playnote(NOTE_F4,quarter);
+  playnote(NOTE_E4,quarter);
+  playnote(NOTE_E4,half);
+  
+  playnote(NOTE_E4,quarter);
+  playnote(NOTE_D4,quarter);
+  playnote(NOTE_D4,quarter);
+  playnote(NOTE_E4,quarter);
+  
+  playnote(NOTE_D4,half);
+  playnote(NOTE_G4,half);
+  
+  /* second verse */
+  playnote(NOTE_E4,quarter);
+  playnote(NOTE_E4,quarter);
+  playnote(NOTE_E4,half);
+  
+  playnote(NOTE_E4,quarter);
+  playnote(NOTE_E4,quarter);
+  playnote(NOTE_E4,half);
+  
+  playnote(NOTE_E4,quarter);
+  playnote(NOTE_G4,quarter);
+  playnote(NOTE_C4,dottedquarter);
+  playnote(NOTE_D4,eighth);
+  
+  playnote(NOTE_E4,whole);
+  
+  playnote(NOTE_F4,quarter);
+  playnote(NOTE_F4,quarter);
+  playnote(NOTE_F4,quarter);
+  playnote(NOTE_F4,quarter);
+  
+  playnote(NOTE_F4,quarter);
+  playnote(NOTE_E4,quarter);
+  playnote(NOTE_E4,quarter);
+  playnote(NOTE_E4,eighth);
+  playnote(NOTE_E4,eighth);
+  
+  playnote(NOTE_G4,quarter);
+  playnote(NOTE_G4,quarter);
+  playnote(NOTE_F4,quarter);
+  playnote(NOTE_D4,quarter);
+  
+  playnote(NOTE_C4,whole);
 }
 
 void playnote(int note, int duration){
