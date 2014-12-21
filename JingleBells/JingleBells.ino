0a46a0b37bfcf08ba8407b09a1f3b491f262dc3c
Magneticdud <Magneticdud@users.noreply.github.com>
2014-12-21 11:56:39 +0100

    Basic functions

--- JingleBells/JingleBells.ino
+++ JingleBells/JingleBells.ino
@@ -4,8 +4,18 @@ int speakerPin = 5;
 void setup() {
   pinMode(speakerPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
+  playmelody();
 }
 
 void loop() {
 
+}
+
+void playmelody(){
+  //
+}
+
+void playnote(int note, int duration){
+  tone(speakerPin,note,duration);
+  delay(duration);
 }
