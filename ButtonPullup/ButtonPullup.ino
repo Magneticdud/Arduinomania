68abefbe4cac8cef9e109919c4372ef020b16ed9
Magneticdud <Magneticdud@users.noreply.github.com>
2014-12-21 16:51:40 +0100

    Morse button!

--- ButtonPullup/ButtonPullup.ino
+++ ButtonPullup/ButtonPullup.ino
@@ -1,9 +1,14 @@
+int speakerPin = 5;
+
 void setup() {
   pinMode(13, OUTPUT); // Use Built-In LED for Indication
   pinMode(12, INPUT_PULLUP);   // Push-Button On Bread Board
+  pinMode(speakerPin, OUTPUT);
 }
  
 void loop() {
   bool buttonState = digitalRead(12);  // store current state of pin 12
-  digitalWrite(13, buttonState);
+  digitalWrite(13, !buttonState);
+  if (!buttonState)
+    tone(speakerPin,1500,10);
 }
