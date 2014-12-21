b2ee6afa6204f827998087cb5e9ba4a46dd7cf96
Magneticdud <Magneticdud@users.noreply.github.com>
2014-12-21 23:46:18 +0100

    Two buttons, two tones

--- ButtonPullup/ButtonPullup.ino
+++ ButtonPullup/ButtonPullup.ino
@@ -11,6 +11,8 @@ void loop() {
   bool buttonState = digitalRead(12);  // store current state of pin 12
   bool buttonState2 = digitalRead(11);
   digitalWrite(13, !buttonState||!buttonState2);
-  if (!buttonState||!buttonState2)
+  if (!buttonState)
     tone(speakerPin,1500,10);
+  else if (!buttonState2)
+    tone(speakerPin,1000,10);
 }
