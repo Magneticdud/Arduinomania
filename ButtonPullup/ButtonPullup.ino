0d5bbfae128e8a985fc87476222273b677ebe0cc
Magneticdud <Magneticdud@users.noreply.github.com>
2014-12-21 23:44:43 +0100

    I have 2 pushbuttons on the breakout board recovered from an old VCR

--- ButtonPullup/ButtonPullup.ino
+++ ButtonPullup/ButtonPullup.ino
@@ -2,13 +2,15 @@ int speakerPin = 5;
 
 void setup() {
   pinMode(13, OUTPUT); // Use Built-In LED for Indication
-  pinMode(12, INPUT_PULLUP);   // Push-Button On Bread Board
+  pinMode(12, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
   pinMode(speakerPin, OUTPUT);
 }
  
 void loop() {
   bool buttonState = digitalRead(12);  // store current state of pin 12
-  digitalWrite(13, !buttonState);
-  if (!buttonState)
+  bool buttonState2 = digitalRead(11);
+  digitalWrite(13, !buttonState||!buttonState2);
+  if (!buttonState||!buttonState2)
     tone(speakerPin,1500,10);
 }
