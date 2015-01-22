cc507c81584b7a2d1d198d8925118f213ab78bf5
Magneticdud <Magneticdud@users.noreply.github.com>
2015-01-22 14:51:13 +0100

    Mandrill begin

--- EthernetShield/EthernetShield.ino
+++ EthernetShield/EthernetShield.ino
@@ -7,6 +7,13 @@ byte mac[] = { 0xFE, 0xDE, 0x2B, 0x89, 0x93, 0xBE };
 byte ip[] = { 192, 168, 10, 177 }; 
 byte mydns[] = { 8, 8, 8, 8 }; 
 byte gateway[] = { 192, 168, 10, 254 }; 
+char server[] = "smtp.mandrillapp.com";
+String UserName64 = "XXXXXXX";
+String Password64 = "XXXXXXX";
+String Sender = "XXXXXXX@x.x";
+String Recipient = "XXXXXXX@x.x";    
+String Subject = "Arduino!!";
+String Body = "Ciao!\nSono Arduino!";
 
 void setup() {
   // put your setup code here, to run once:
