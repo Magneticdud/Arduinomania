a8844629379fcae5bcf73c717ee330e84eea9a3e
Magneticdud <Magneticdud@users.noreply.github.com>
2015-01-22 14:38:42 +0100

    Initial code. Must use "mydns" as a variable name, can't use "dns"

--- EthernetShield/EthernetShield.ino
+++ EthernetShield/EthernetShield.ino
@@ -1,6 +1,16 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+// the media access control (ethernet hardware) address for the shield:
+byte mac[] = { 0xFE, 0xDE, 0x2B, 0x89, 0x93, 0xBE };  
+//the IP address for the shield:
+byte ip[] = { 192, 168, 10, 177 }; 
+byte mydns[] = { 8, 8, 8, 8 }; 
+byte gateway[] = { 192, 168, 10, 254 }; 
+
 void setup() {
   // put your setup code here, to run once:
-
+  Ethernet.begin(mac, ip, mydns, gateway);
 }
 
 void loop() {
