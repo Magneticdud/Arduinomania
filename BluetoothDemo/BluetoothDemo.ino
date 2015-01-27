590b736086c6c4f3d0dfda134425ba1ef85e98a2
Magneticdud <Magneticdud@users.noreply.github.com>
2015-01-28 00:06:16 +0100

    Serial demo

--- BluetoothDemo/BluetoothDemo.ino
+++ BluetoothDemo/BluetoothDemo.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
-
+//disconnect hc-06 when uploading
+String message; //string that stores the incoming message
+ 
+void setup()
+{
+  Serial.begin(9600); //set baud rate
 }
+ 
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  while(Serial.available())
+  {//while there is data available on the serial monitor
+    message+=char(Serial.read());//store string from serial command
+  }
+  if(!Serial.available())
+  {
+    if(message!="")
+    {//if data is available
+      Serial.println(message); //show the data
+      message=""; //clear the data
+    }
+  }
+  delay(5000); //delay
 }
