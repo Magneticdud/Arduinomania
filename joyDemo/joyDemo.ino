4c80e1a5117fe3a662adaa4bd840bc67aaf29e7d
Magneticdud <Magneticdud@users.noreply.github.com>
2015-02-03 13:58:22 +0100

    Playstation pad on Arduino

---
+++ joyDemo/joyDemo.ino
@@ -0,0 +1,12 @@
+int xPin = A0;
+int yPin = A1;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); //set baud rate for USB serial
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
