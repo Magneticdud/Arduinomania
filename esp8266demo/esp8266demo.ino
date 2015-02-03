41a28c564c5d5d5a61756730dc1d29cce6488843
Magneticdud <Magneticdud@users.noreply.github.com>
2015-02-04 00:36:48 +0100

    Print ip address

--- esp8266demo/esp8266demo.ino
+++ esp8266demo/esp8266demo.ino
@@ -12,6 +12,8 @@ void setup() {
   bool b = wifi.Initialize(STA, SSID, PASSWORD);
   delay(8000);  //make sure the module can have enough time to get an IP address 
   String ipstring  = wifi.showIP();
+  DebugSerial.print("My IP address: ");
+  DebugSerial.println(ipstring);
 }
 
 void loop() {
