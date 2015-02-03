d59b5c6d6d16fab0b8255f98cd46695c5fa89e04
Magneticdud <Magneticdud@users.noreply.github.com>
2015-02-04 00:35:36 +0100

    Works

--- esp8266demo/esp8266demo.ino
+++ esp8266demo/esp8266demo.ino
@@ -1,6 +1,17 @@
+//uartwifi.h must be changed according if using MEGA or UNO. I'm using MEGA
+#define SSID       "myrealssid"
+#define PASSWORD   "mysupersecretpassword"
+
+#include "uartWIFI.h"
+#include <SoftwareSerial.h>
+WIFI wifi;
+
 void setup() {
   // put your setup code here, to run once:
-
+  wifi.begin();
+  bool b = wifi.Initialize(STA, SSID, PASSWORD);
+  delay(8000);  //make sure the module can have enough time to get an IP address 
+  String ipstring  = wifi.showIP();
 }
 
 void loop() {
