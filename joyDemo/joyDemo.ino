cc075cd4363d19b3e9bdd9d31e95e18ee1f9394e
Magneticdud <Magneticdud@users.noreply.github.com>
2015-02-03 14:36:03 +0100

    Pad to serial

--- joyDemo/joyDemo.ino
+++ joyDemo/joyDemo.ino
@@ -1,12 +1,22 @@
 int xPin = A0;
 int yPin = A1;
+int xpos = 0;
+int ypos = 0;
+String outserial = "";
+String xtring = "X ";
+String ytring = " Y ";
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600); //set baud rate for USB serial
+  Serial.println("Serial start");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  xpos=analogRead(xPin);
+  ypos=analogRead(yPin);
+  outserial = xtring + xpos + ytring + ypos;
+  Serial.println(outserial);
+  delay(100);
 }
