7c09353b188b4d7bf21ec43d74decfe06afb8600
Magneticdud <Magneticdud@users.noreply.github.com>
2015-02-03 15:08:46 +0100

    Instructions

--- KeypadDemo/KeypadDemo.ino
+++ KeypadDemo/KeypadDemo.ino
@@ -1,5 +1,16 @@
 //Demo for Arduino analog keypad breakout
 //$3.25 from here http://bit.ly/1ysFtHD
+//readings:
+//L 0
+//R 500
+//UP 141
+//DOWN 325-326
+//button 738
+//nothing = 1023
+//allow some tolerance (+-5? or probably it will fail reading when the switch is worn out
+//double press doesn't work: it's just a resistor circuit,
+//the current flows in the path of less resistance
+//the button with the lowest value will always prevail in double press
 
 int keyPin = A0;
 
