2a278ebdfd8eb454797e2766388a180e173d7653
Magneticdud <Magneticdud@users.noreply.github.com>
2015-04-29 00:26:45 +0200

    let's relay

---
+++ MiniRelay/MiniRelay.ino
@@ -0,0 +1,12 @@
+// the setup function runs once when you press reset or power the board
+void setup() {
+  pinMode(7, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(7, HIGH);
+  delay(1000);
+  digitalWrite(, LOW);
+  delay(1000);
+}
