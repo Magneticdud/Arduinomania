4d2af5079110dd57b78147013cbbeef9866248d9
Magneticdud <Magneticdud@users.noreply.github.com>
2016-02-22 15:29:50 +0100

    Firmata update

--- MediaPortalRemote/keys.h
+++ MediaPortalRemote/keys.h
@@ -1,107 +1,107 @@
 //keys to press on Transmission, and IR hex code on Receiving
 //receiving codes from a MCE Remote
 
-const int leftclickR;
-const int leftclickT;
-const int rightclickR;
-const int rightclickT;
-const int windowskeyR;
-const int windowskeyT;
+const int leftclickR = 0;
+const int leftclickT = 0;
+const int rightclickR = 0;
+const int rightclickT = 0;
+const int windowskeyR = 0;
+const int windowskeyT = 0;
 //send ctrl+d to make the auto3d menu appear
-const int auto3dswitchR;
-const int auto3dswitchT;
-const int leftarrowR;
-const int leftarrowT;
-const int rightarrowR;
-const int rightarrowT;
-const int uparrowR;
-const int uparrowT;
-const int downarrowR;
-const int downarrowT;
+const int auto3dswitchR = 0;
+const int auto3dswitchT = 0;
+const int leftarrowR = 0;
+const int leftarrowT = 0;
+const int rightarrowR = 0;
+const int rightarrowT = 0;
+const int uparrowR = 0;
+const int uparrowT = 0;
+const int downarrowR = 0;
+const int downarrowT = 0;
 //home is H on mediaportal
-const int homeR;
-const int homeT;
-const int enterR;
-const int enterT;
+const int homeR = 0;
+const int homeT = 0;
+const int enterR = 0;
+const int enterT = 0;
 //pause is spacebar on mediaportal
-const int spaceR;
-const int spaceT;
+const int pauseR = 0;
+const int pauseT = 0;
 //first item is home key
-const int firstR;
-const int firstT;
+const int firstR = 0;
+const int firstT = 0;
 //last item is end key
-const int lastR;
-const int lastT;
+const int lastR = 0;
+const int lastT = 0;
 //pgup is previous page
-const int previouspageR;
-const int previouspageT;
+const int previouspageR = 0;
+const int previouspageT = 0;
 //pgdown is next page
-const int nextpageR;
-const int nextpageT;
+const int nextpageR = 0;
+const int nextpageT = 0;
 //M is mute
-const int muteR;
-const int muteT;
+const int muteR = 0;
+const int muteT = 0;
 //P is play all
-const int playallR;
-const int playallT;
+const int playallR = 0;
+const int playallT = 0;
 //B is stop
-const int stopR;
-const int stopT;
+const int stopR = 0;
+const int stopT = 0;
 //F3 is show info
-const int showinfoR;
-const int showinfoT;
+const int showinfoR = 0;
+const int showinfoT = 0;
 //F5 is rewind
-const int rewindR;
-const int rewindT;
+const int rewindR = 0;
+const int rewindT = 0;
 //F6 is Fast forward
-const int ffR;
-const int ffT;
+const int ffR = 0;
+const int ffT = 0;
 //F7 is Play previous
-const int playpreviousR;
-const int playpreviousT;
+const int playpreviousR = 0;
+const int playpreviousT = 0;
 //F8 is Play next
-const int playnextR;
-const int playnextT;
+const int playnextR = 0;
+const int playnextT = 0;
 //F9 is show context menu
-const int showcontextR;
-const int showcontextT;
-const int ESCR;
-const int ESCT;
-const int ALTF4R;
-const int ALTF4T;
+const int showcontextR = 0;
+const int showcontextT = 0;
+const int ESCR = 0;
+const int ESCT = 0;
+const int ALTF4R = 0;
+const int ALTF4T = 0;
 // U is go to Parent folder
-const int parentfolderR;
-const int parentfolderT;
+const int parentfolderR = 0;
+const int parentfolderT = 0;
 // X Switch between MediaPortal GUI and the currently playing media, such as fullscreen tv, video, visualization or Now Playing
-const int PIPR;
-const int PIPT;
+const int PIPR = 0;
+const int PIPT = 0;
 // R records
-const int recR;
-const int recT;
-const int key1R;
-const int key1T;
-const int key2R;
-const int key2T;
-const int key3R;
-const int key3T;
-const int key4R;
-const int key4T;
-const int key5R;
-const int key5T;
-const int key6R;
-const int key6T;
-const int key7R;
-const int key7T;
-const int key8R;
-const int key8T;
-const int key9R;
-const int key9T;
+const int recR = 0;
+const int recT = 0;
+const int key1R = 0;
+const int key1T = 0;
+const int key2R = 0;
+const int key2T = 0;
+const int key3R = 0;
+const int key3T = 0;
+const int key4R = 0;
+const int key4T = 0;
+const int key5R = 0;
+const int key5T = 0;
+const int key6R = 0;
+const int key6T = 0;
+const int key7R = 0;
+const int key7T = 0;
+const int key8R = 0;
+const int key8T = 0;
+const int key9R = 0;
+const int key9T = 0;
 //0 is also used to delete recordings and the last channel, consider adding a duplicate key?
-const int key0R;
-const int key0T;
+const int key0R = 0;
+const int key0T = 0;
 //Y is show/hide OSD
-const int osdshowR;
-const int osdshowT;
+const int osdshowR = 0;
+const int osdshowT = 0;
 //F1 reset to default
-const int defaultR;
-const int defaultT;
+const int defaultR = 0;
+const int defaultT = 0;
